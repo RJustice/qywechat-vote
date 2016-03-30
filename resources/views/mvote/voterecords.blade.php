1417d797eb80669cd1e3b3c96effdcd001a8b771
Ivan <ivanyun.t@gmail.com>
2016-03-30 15:18:46 +0800

    f

--- resources/views/mvote/voterecords.blade.php
+++ resources/views/mvote/voterecords.blade.php
@@ -5,13 +5,22 @@
     <div class="row">
         <div class="col-md-12">
             <h3 class="text-center">{{ $vote->title }}</h3>
-            <div class="btn-group">
-                <a href="{{ url('manage/vote/statistics',['id'=>$vote->id,'order'=>'asc']) }}" class="btn btn-default">由低到高</a>
-                <a href="{{ url('manage/vote/statistics',['id'=>$vote->id,'order'=>'desc']) }}" class="btn  btn-default">由高到低</a>
-                <a href="{{ url('manage/vote/records',['id'=>$vote->id]) }}" class="btn btn-success">投票人统计</a>
-            </div>
-            <div class="btn-group">
+            <div class="btn-group pull-right">
                 <a href="{{ url('manage/vote/list') }}" class="btn btn-default">返回评测列表</a>
+            </div>
+            <div class="btn-group pull-right">
+                <a href="{{ url('manage/vote/'.$r,['id'=>$vote->id,'order'=>'asc']) }}" class="btn @if($order == '' || $order == 'asc') btn-success @else btn-default @endif">由低到高</a>
+                <a href="{{ url('manage/vote/'.$r,['id'=>$vote->id,'order'=>'desc']) }}" class="btn @if($order == 'desc') btn-success @else btn-default @endif">由高到低</a>
+            </div>
+            <div class="btn-group pull-left" style="margin-right:30px;">
+                <a href="{{ url('manage/vote/records',['id'=>$vote->id]) }}" class="btn btn-default">投票人统计</a>
+            </div>
+            <div class="btn-group pull-left">            
+                <a href="{{ url('manage/vote/statistics',['id'=>$vote->id,'order'=>'desc']) }}" class="btn btn-default @if($r == 'statistics') btn-success @endif">全部排名</a>
+                <a href="{{ url('manage/vote/youxiu',['id'=>$vote->id,'order'=>'desc']) }}" class="btn btn-default @if($r == 'youxiu') btn-success @endif">优秀排名</a>
+                <a href="{{ url('manage/vote/lianghao',['id'=>$vote->id,'order'=>'desc']) }}" class="btn btn-default @if($r == 'lianghao') btn-success @endif">良好排名</a>
+                <a href="{{ url('manage/vote/hege',['id'=>$vote->id,'order'=>'desc']) }}" class="btn btn-default @if($r == 'hege') btn-success @endif">合格排名</a>
+                <a href="{{ url('manage/vote/buhege',['id'=>$vote->id,'order'=>'desc']) }}" class="btn btn-default @if($r == 'buhege') btn-success @endif">不合格排名</a>
             </div>
         </div>
         <br /><br />
