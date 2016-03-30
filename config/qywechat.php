0d7a2f2f4c94cb07a99e73801fa15a0cad66c281
Ivan <ivanyun.t@gmail.com>
2016-03-30 10:40:16 +0800

    f

--- config/qywechat.php
+++ config/qywechat.php
@@ -6,6 +6,6 @@ return [
         'appid' => 'wxaeff15efe968850c',
         'appsecret' => '9NEi0eWG8im-L_amE_bdw5CsWrSf9Pw3Af-z77fHg7sv_mcxEN7kOxIC0XCAe8Uq',
         // 'encodingaeskey' => false,
-        'agentid' => 46,
+        'agentid' => 4,
     ],
 ];
