987aac6aabdd68d1babd247e78b106bd6fb6135c
Ivan <ivanyun.t@gmail.com>
2016-05-03 18:02:07 +0800

    g

---
+++ resources/views/vote/chaoshi.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="alert alert-danger text-center">
+                <h3>该民意评测时间已过</h3>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+@endsection
