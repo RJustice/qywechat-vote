ccfe2844f84cf3102b98985e7c84f12f68b03446
Ivan <ivanyun.t@gmail.com>
2016-05-24 10:21:08 +0800

    f

--- resources/views/mvote/vlist.blade.php
+++ resources/views/mvote/vlist.blade.php
@@ -4,10 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            
+            <?php if( Auth::user()->id === 1 ): ?>
             <div class="btn-group">                
                 <a href="{{ url('manage/vote/create') }}" class="btn btn-info">创建评测</a>
             </div>
-            <?php if( Auth::user()->id === 1 ): ?>
             <div class="btn-group">                
                 <a href="{{ url('manage/users') }}" class="btn btn-info">管理员</a>
             </div>
@@ -19,11 +20,12 @@
                 <a href="{{ url('manage/users/'.Auth::user()->id.'/edit') }}" class="btn btn-info">修改管理密码</a>
             </div>
             <?php endif;?>
-
+            <?php if( Auth::user()->id === 1 ): ?>
             <div class="btn-group pull-right">
                 <a href="{{ url('manage/sync') }}" class="btn btn-default" id="sync">同步通讯录</a>
                 <!-- <a href="{{ url('manage/role') }}" class="btn btn-default" id="role">微信统计权限</a> -->
             </div>
+            <?php endif;?>
         </div>
     </div>
     <div class="row">
@@ -37,9 +39,11 @@
                         
                         <span class="btn-group pull-right">
                             <a href="{{ url('manage/vote/statistics',['id'=>$v->id,'order'=>'asc']) }}" class="btn btn-primary">查看统计</a>
+                            <?php if( Auth::user()->id === 1 ): ?>
                             <a href="{{ url('manage/vote/edit',['id'=>$v->id]) }}" class="btn btn-primary">编辑</a>
                             <a href="javascript:;" class="btn btn-primary" onclick="delV({{ $v->id }})">删除</a>
                             <a href="{{ url('manage/vote/copy',['id'=>$v->id]) }}" class="btn btn-primary">复制</a>
+                            <?php endif;?>
                         </span>
                     </p>
                 </li>
